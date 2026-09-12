a7c088d84a6b09903133dcc43ec29509d9c63254
SabinaCSC <sabinaakanko@gmail.com>
2026-09-12 21:35:20 +0000

    Refactor score input and update page configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,27 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="UDS Grade Checker",
+    page_icon="uds logo.png",
+    layout="centered"
+)
+
+st.image("uds logo.png", width=120)
+
 st.title("🎓 UDS Grade Checker")
+st.caption("Developed by Sabina Akanko • BSc Computer Science • UDS Nyankpala")
 
 name = st.text_input("Enter your name")
-score = st.number_input("Enter your score", min_value=0, max_value=100, step=1)
+
+score = st.number_input(
+    "Enter your score",
+    min_value=0,
+    max_value=100,
+    step=1
+)
 
 if st.button("Check Grade"):
+
     if score >= 80:
         grade = "Grade A"
     elif score >= 75:
